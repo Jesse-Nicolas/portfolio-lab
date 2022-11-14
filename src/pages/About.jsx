85c9e4d64e512e8dee929542f1d54f628a95f859
Jesse Nicolas <jn@nevada.unr.edu>
2022-11-14 14:49:07 -0800

    about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,22 @@
+import '../styles/about.css'
+import winery from '../assets/winery.jpg'
+import truck from '../assets/truck.jpg'
+import tahoe from '../assets/tahoe.jpg'
+
 const About = () => {
-  return (  
-    <h1>About Page</h1>
+  return ( 
+    <>
+      <h1>About Me</h1>
+      <div className='branding-statement'>
+        <p>I am a dedicated problem solver and lover of fixing things, seeking a role as a software engineer. I enjoy working with and communicating complex ideas to build practical, user-focused products. My experience in education, logic, and ethics has allowed me to develop the skills to be an effective engineer and teammate. I have a deep appreciation for elegant solutions and designs, and I strive to create in a way that reflects such minimalism and utility.</p>
+      </div>
+      <div id='photos-container'>
+        <img src={tahoe} alt="tahoe fire lookout" />
+        <img src={winery} alt="at a winery" />
+        {/* <img src={truck} alt="the truck" /> */}
+      </div>
+    </> 
   );
 }
- 
+
 export default About
