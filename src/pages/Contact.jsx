8f38c7bcf26add0accda9abebbc386f63d9a1804
Jesse Nicolas <jn@nevada.unr.edu>
2022-11-15 03:34:41 -0800

    resume download link

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -6,13 +6,13 @@ const Contact = () => {
   return (  
     <>
       <h1>Contact</h1>
+      <h3>email: <a href="mailto:jessegnicolas@outlook.com">jessegnicolas@outlook.com</a></h3>
       <div className='links'>
         <a href="https://github.com/Jesse-Nicolas">
           <div className='img-container' style={{backgroundImage: `url(${github})`}}>
             <h2 id='light'>github</h2>
           </div>
         </a>
-
         <a href="https://www.linkedin.com/in/jesse-nicolas/">
           <div className='img-container' style={{backgroundImage: `url(${linkedin})`}}>
             <h2 id='dark'>LinkedIn</h2>
--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,6 +1,11 @@
+import resume from '../assets/resume_jesse_nicolas.pdf'
+
 const Resume = () => {
   return (  
-    <h1>Resume Page</h1>
+    <>
+      <h1>Resume</h1>
+      <h3><a href={resume} download>Download</a></h3>
+    </>
   );
 }
 
