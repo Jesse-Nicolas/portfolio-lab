887094a8e4fc4b26dd59df20c1753d3bfbad2e49
Jesse Nicolas <jn@nevada.unr.edu>
2022-11-14 16:31:46 -0800

    nice external links

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,6 +1,26 @@
+import '../styles/contact.css'
+import github from '../assets/github.png'
+import linkedin from '../assets/linkedin.png'
+
 const Contact = () => {
   return (  
-    <h1>Contact</h1>
+    <>
+      <h1>Contact</h1>
+      <div className='links'>
+        <a href="https://github.com/Jesse-Nicolas">
+          <div className='img-container' style={{backgroundImage: `url(${github})`}}>
+            <h2 id='light'>github</h2>
+          </div>
+        </a>
+
+        <a href="https://www.linkedin.com/in/jesse-nicolas/">
+          <div className='img-container' style={{backgroundImage: `url(${linkedin})`}}>
+            <h2 id='dark'>LinkedIn</h2>
+          </div>
+        </a>
+
+      </div>
+    </>
   );
 }
 
