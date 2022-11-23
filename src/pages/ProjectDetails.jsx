4acd03cc0624540a64e7244575f83bc69ea7b7e9
Jesse Nicolas <jn@nevada.unr.edu>
2022-11-23 09:29:16 -0800

    fix projectdetails

--- src/pages/ProjectDetails.jsx
+++ src/pages/ProjectDetails.jsx
@@ -1,3 +1,4 @@
+import '../styles/ProjectDetails.css'
 import { find } from "../utilities/findProject";
 import { useParams } from "react-router-dom";
 import Footer from "../Components/Footer";
@@ -9,11 +10,15 @@ const ProjectDetails = () => {
 
   return (  
     <>
-      <h1>{project.title}</h1>
-      <p>{project.description}</p>
-      <img src={project.image} alt="project screenshot" />
-      <br/><a href={project.repositoryLink}>Github Repo</a> 
-      <br/><a href={project.deploymentLink}>Deployed site</a>
+      <div className='details'>
+        <h1>{project.title}</h1>
+        <p>{project.description}</p>
+        <img className='big-image' src={project.image} alt="project screenshot" />
+      </div>
+      <div className='details-links'>
+        <a className='link' href={project.repositoryLink}>Github Repo</a> 
+        <a className='link' href={project.deploymentLink}>Deployed site</a>
+      </div>
       <div className='footer'>
         <Footer />
       </div>
