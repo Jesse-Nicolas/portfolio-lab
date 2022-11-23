5073da2733f5448482cdd6b7b300a172b30461e7
Jesse Nicolas <jn@nevada.unr.edu>
2022-11-23 09:58:05 -0800

    fix resume page

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,11 +1,13 @@
+import '../styles/Resume.css'
 import resume from '../assets/resume_jesse_nicolas.pdf'
 import Footer from '../Components/Footer';
 
 const Resume = () => {
   return (  
     <>
-      <h1>Resume</h1>
-      <h3><a href={resume} download>Download</a></h3>
+      <object data={resume} type="application/pdf" width="100%" height="100%">
+        <p>This is the alt text for my resume</p>
+      </object>
       <div className='footer'>
         <Footer />
       </div>
