5b30cf0198a60b56e861881b86b54a52d5873ad2
Jesse Nicolas <jn@nevada.unr.edu>
2022-11-28 09:00:42 -0800

    fix resume embedding on mobile

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,11 +1,12 @@
 import '../styles/Resume.css'
 import resume from '../assets/resume_jesse_nicolas.pdf'
+import resumePng from '../assets/Resume.png'
 
 const Resume = () => {
   return (  
     <>
       <object data={resume} type="application/pdf" width="100%" height="100%">
-        <p>alt text for resume</p>
+        <img className='resume-img' src={resumePng} alt="resume" />
       </object>
     </>
   );
