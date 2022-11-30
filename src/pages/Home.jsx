f4b7036f21c261b7ea4bc575bdadf82c7c0c860e
Jesse Nicolas <jn@nevada.unr.edu>
2022-11-30 08:39:10 -0800

    fix home page and navbar styling for mobile

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,10 @@ const Home = () => {
       <div className='page-content'>
         <div id='intro'>
           <img id="homepicture" src={reclining} alt="reclining in yellowstone" />
-          <p className="typewriter">I'm Jesse, a full-stack Software Engineer.</p>
+          <div id='welcome'>
+            <p>I'm Jesse,</p>
+            <p className="typewriter">full-stack Software Engineer.</p>
+          </div>
         </div>
       </div>
     </>
