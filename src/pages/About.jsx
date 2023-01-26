75b503e8c83be9a8205476d0462a4f99432b40ce
Jesse Nicolas <jn@nevada.unr.edu>
2023-01-26 07:47:29 -0800

    update resume and bio

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -7,8 +7,7 @@ const About = () => {
       <h1>About Me</h1>
       <div className="about">
         <div className='branding-statement'>
-          <p>I am a dedicated problem solver and lover of fixing things, seeking a role as a software engineer. </p><p>I enjoy working with and communicating complex ideas to build practical, user-focused products. My experience in education, logic, and ethics has allowed me to develop the skills to be an effective engineer and teammate.</p>
-          <p> I have a deep appreciation for elegant solutions and designs, and I strive to create in a way that reflects such minimalism and utility.</p>
+          <p>I’m a recent grad seeking a challenging software engineering job, and am excited to grow my technical abilities. I enjoy approaching a problem by peeling back layers of complexity to understand how a thing works and why it was designed the way it was, so that improvements can be made.</p><p> I started practicing this with poorly running engines over ten years ago, later on with complex long-form arguments in philosophy essays, and was even recognized for my logical reasoning skills when hired as my university’s Logic Tutor. Software engineering is the professional path that merges the communication and soft skills honed in my philosophy education with the methodical problem solving I’ve practiced for years, and I’m confident I would be a valuable investment in your engineering team.</p>
         </div>
         <div id='photos-container'>
           <img src={winery} alt="at a winery" />
