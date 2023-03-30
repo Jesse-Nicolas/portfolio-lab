b089dc791e7ed032e8c4b44ede56422e4a934ddb
Jesse Nicolas <jn@nevada.unr.edu>
2023-03-30 09:57:03 -0700

    update about me blurb

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -7,7 +7,7 @@ const About = () => {
       <h1>About Me</h1>
       <div className="about">
         <div className='branding-statement'>
-          <p>I’m a recent grad seeking a challenging software engineering job, and am excited to grow my technical abilities. I enjoy approaching a problem by peeling back layers of complexity to understand how a thing works and why it was designed the way it was, so that improvements can be made.</p><p> I started practicing this with poorly running engines over ten years ago, later on with complex long-form arguments in philosophy essays, and was even recognized for my logical reasoning skills when hired as my university’s Logic Tutor. Software engineering is the professional path that merges the communication and soft skills honed in my philosophy education with the methodical problem solving I’ve practiced for years, and I’m confident I would be a valuable investment in your engineering team.</p>
+          <p>Hi, I'm Jesse. I use tech like JavaScript, React, Node, Python, and Django to build full stack applications with SQL and noSQL databases. My background studying and tutoring Philosophy/Logic has proven my critical thinking, communication, and analytic skills; and developed my deep understanding of ethics and empathy. Fixing and tuning engines for over a decade has given me a unique sense for adaptation and resourcefulness, and a creative approach to problem solving. </p>
         </div>
         <div id='photos-container'>
           <img src={winery} alt="at a winery" />
